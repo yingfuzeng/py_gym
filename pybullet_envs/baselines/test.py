aa8156327f60f75a0f51cb5004b96aeffb879c81
yingfu <yingfuzeng@gmail.com>
2019-12-11 13:33:35 +0800

    simple test

--- pybullet_envs/baselines/test.py
+++ pybullet_envs/baselines/test.py
@@ -1,24 +1,24 @@
 import gym
 import numpy as np
 
-from stable_baselines import TD3
-from stable_baselines.td3.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines.ddpg.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from stable_baselines.ddpg.policies import MlpPolicy
+from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
+from stable_baselines import DDPG
 
-env = gym.make('Pendulum-v0')
+env = gym.make('MountainCarContinuous-v0')
 
-# The noise objects for TD3
+# the noise objects for DDPG
 n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+param_noise = None
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
 
-model = TD3(MlpPolicy, env, action_noise=action_noise, verbose=1)
-model.learn(total_timesteps=50000, log_interval=10)
-model.save("td3_pendulum")
+model = DDPG(MlpPolicy, env, verbose=1, param_noise=param_noise, action_noise=action_noise)
+model.learn(total_timesteps=400000)
+model.save("ddpg_mountain")
 
 del model # remove to demonstrate saving and loading
 
-model = TD3.load("td3_pendulum")
+model = DDPG.load("ddpg_mountain")
 
 obs = env.reset()
 while True:
